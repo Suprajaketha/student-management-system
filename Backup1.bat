a64182136ae64e307cad711507fa77ae4edfb7d3
Suprajaketha <138648904+Suprajaketha@users.noreply.github.com>
2024-02-22 16:29:17 +0530

    Update Backup1.bat

--- Backup1.bat
+++ Backup1.bat
@@ -1,6 +1,7 @@
 @echo off
 set source_folder=C:\Users\Suparaja\Documents\Test01
 set backup_folder=Backup1
+set zip_file=backup.zip
 
 echo Source folder: %source_folder%
 echo Backup folder: %backup_folder%
@@ -20,3 +21,8 @@ rem Copy all files from source folder to backup folder
 xcopy /s /e /i "%source_folder%\*" "%backup_folder%"
 
 echo Files copied successfully.
+
+rem Zip the backup folder using .NET Framework classes
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::CreateFromDirectory('%backup_folder%', '%zip_file%')"
+
+echo Backup created and zipped successfully.
