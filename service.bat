c7e7700594764eac8446ef83d0dc68a707200c98
Suprajaketha <138648904+Suprajaketha@users.noreply.github.com>
2024-03-06 11:16:28 +0530

    Update service.bat

--- service.bat
+++ service.bat
@@ -1,7 +1,15 @@
 @echo off
 
-:: Stop AnyDesk service
-sc stop AnyDesk
+:: Stop AnyDesk service if it's running
+sc query AnyDesk | find "STATE" | find "RUNNING" > nul
+if %errorlevel% equ 0 (
+    sc stop AnyDesk
+)
+
+:: Path to AnyDesk executable
+set "AnyDeskPath=C:\Program Files (x86)\AnyDesk\AnyDesk.exe"
+
+pause
 
 :: Delete file in Backup folder
 del "C:\Users\Suparaja\Documents\Backup\Test 5.txt"
